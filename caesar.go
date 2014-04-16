a0658b812fbd2068178cc302622ad3ad51d069e8
Alex Jackson <alex@alexj.org>
2014-04-17 00:22:46 +0100

    Added documentation

--- caesar.go
+++ caesar.go
@@ -1,26 +1,30 @@
+// Package caesar provides a simple way to encrypt and decrypt messages using the Caesar cipher.
+// It works with UTF-8 strings but can only encrypt/decrypt the latin ASCII characters ([A..Z], [a..z]) in a string.
 package caesar
 
 const lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
 const upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+// Encrypts a plaintext string by shifting each character with the provided key
 func EncryptPlaintext(plaintext string, key int) string {
-	return rotateText(plaintext, key)
+  return rotateText(plaintext, key)
 }
 
+// Decrypts ciphertext by reverse shifting each character in the ciphertext with the key provided
 func DecryptCiphertext(ciphertext string, key int) string {
-	return rotateText(ciphertext, -key)
+  return rotateText(ciphertext, -key)
 }
 
 func rotateText(inputText string, rot int) string {
-	rot %= 26
-	rotatedText := []byte(inputText)
+  rot %= 26
+  rotatedText := []byte(inputText)
 
-	for index, byteValue := range rotatedText {
-		if byteValue >= 'a' && byteValue <= 'z' {
-			rotatedText[index] = lowerCaseAlphabet[(int((26+(byteValue-'a')))+rot)%26]
-		} else if byteValue >= 'A' && byteValue <= 'Z' {
-			rotatedText[index] = upperCaseAlphabet[(int((26+(byteValue-'A')))+rot)%26]
-		}
-	}
-	return string(rotatedText)
+  for index, byteValue := range rotatedText {
+    if byteValue >= 'a' && byteValue <= 'z' {
+      rotatedText[index] = lowerCaseAlphabet[(int((26+(byteValue-'a')))+rot)%26]
+    } else if byteValue >= 'A' && byteValue <= 'Z' {
+      rotatedText[index] = upperCaseAlphabet[(int((26+(byteValue-'A')))+rot)%26]
+    }
+  }
+  return string(rotatedText)
 }
--- caesar_test.go
+++ caesar_test.go
@@ -1,59 +1,75 @@
-package caesar
+package caesar_test
 
 import (
-	"strings"
-	"testing"
+  "fmt"
+  "github.com/alexjohnj/caesar"
+  "strings"
+  "testing"
 )
 
 const testingString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var expectedEncryptionResults map[int]string = map[int]string{
-	1:  "bcdefghijklmnopqrstuvwxyzaBCDEFGHIJKLMNOPQRSTUVWXYZA",
-	2:  "cdefghijklmnopqrstuvwxyzabCDEFGHIJKLMNOPQRSTUVWXYZAB",
-	3:  "defghijklmnopqrstuvwxyzabcDEFGHIJKLMNOPQRSTUVWXYZABC",
-	4:  "efghijklmnopqrstuvwxyzabcdEFGHIJKLMNOPQRSTUVWXYZABCD",
-	5:  "fghijklmnopqrstuvwxyzabcdeFGHIJKLMNOPQRSTUVWXYZABCDE",
-	6:  "ghijklmnopqrstuvwxyzabcdefGHIJKLMNOPQRSTUVWXYZABCDEF",
-	7:  "hijklmnopqrstuvwxyzabcdefgHIJKLMNOPQRSTUVWXYZABCDEFG",
-	8:  "ijklmnopqrstuvwxyzabcdefghIJKLMNOPQRSTUVWXYZABCDEFGH",
-	9:  "jklmnopqrstuvwxyzabcdefghiJKLMNOPQRSTUVWXYZABCDEFGHI",
-	10: "klmnopqrstuvwxyzabcdefghijKLMNOPQRSTUVWXYZABCDEFGHIJ",
-	11: "lmnopqrstuvwxyzabcdefghijkLMNOPQRSTUVWXYZABCDEFGHIJK",
-	12: "mnopqrstuvwxyzabcdefghijklMNOPQRSTUVWXYZABCDEFGHIJKL",
-	13: "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM",
-	14: "opqrstuvwxyzabcdefghijklmnOPQRSTUVWXYZABCDEFGHIJKLMN",
-	15: "pqrstuvwxyzabcdefghijklmnoPQRSTUVWXYZABCDEFGHIJKLMNO",
-	16: "qrstuvwxyzabcdefghijklmnopQRSTUVWXYZABCDEFGHIJKLMNOP",
-	17: "rstuvwxyzabcdefghijklmnopqRSTUVWXYZABCDEFGHIJKLMNOPQ",
-	18: "stuvwxyzabcdefghijklmnopqrSTUVWXYZABCDEFGHIJKLMNOPQR",
-	19: "tuvwxyzabcdefghijklmnopqrsTUVWXYZABCDEFGHIJKLMNOPQRS",
-	20: "uvwxyzabcdefghijklmnopqrstUVWXYZABCDEFGHIJKLMNOPQRST",
-	21: "vwxyzabcdefghijklmnopqrstuVWXYZABCDEFGHIJKLMNOPQRSTU",
-	22: "wxyzabcdefghijklmnopqrstuvWXYZABCDEFGHIJKLMNOPQRSTUV",
-	23: "xyzabcdefghijklmnopqrstuvwXYZABCDEFGHIJKLMNOPQRSTUVW",
-	24: "yzabcdefghijklmnopqrstuvwxYZABCDEFGHIJKLMNOPQRSTUVWX",
-	25: "zabcdefghijklmnopqrstuvwxyZABCDEFGHIJKLMNOPQRSTUVWXY",
-	26: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  1:  "bcdefghijklmnopqrstuvwxyzaBCDEFGHIJKLMNOPQRSTUVWXYZA",
+  2:  "cdefghijklmnopqrstuvwxyzabCDEFGHIJKLMNOPQRSTUVWXYZAB",
+  3:  "defghijklmnopqrstuvwxyzabcDEFGHIJKLMNOPQRSTUVWXYZABC",
+  4:  "efghijklmnopqrstuvwxyzabcdEFGHIJKLMNOPQRSTUVWXYZABCD",
+  5:  "fghijklmnopqrstuvwxyzabcdeFGHIJKLMNOPQRSTUVWXYZABCDE",
+  6:  "ghijklmnopqrstuvwxyzabcdefGHIJKLMNOPQRSTUVWXYZABCDEF",
+  7:  "hijklmnopqrstuvwxyzabcdefgHIJKLMNOPQRSTUVWXYZABCDEFG",
+  8:  "ijklmnopqrstuvwxyzabcdefghIJKLMNOPQRSTUVWXYZABCDEFGH",
+  9:  "jklmnopqrstuvwxyzabcdefghiJKLMNOPQRSTUVWXYZABCDEFGHI",
+  10: "klmnopqrstuvwxyzabcdefghijKLMNOPQRSTUVWXYZABCDEFGHIJ",
+  11: "lmnopqrstuvwxyzabcdefghijkLMNOPQRSTUVWXYZABCDEFGHIJK",
+  12: "mnopqrstuvwxyzabcdefghijklMNOPQRSTUVWXYZABCDEFGHIJKL",
+  13: "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM",
+  14: "opqrstuvwxyzabcdefghijklmnOPQRSTUVWXYZABCDEFGHIJKLMN",
+  15: "pqrstuvwxyzabcdefghijklmnoPQRSTUVWXYZABCDEFGHIJKLMNO",
+  16: "qrstuvwxyzabcdefghijklmnopQRSTUVWXYZABCDEFGHIJKLMNOP",
+  17: "rstuvwxyzabcdefghijklmnopqRSTUVWXYZABCDEFGHIJKLMNOPQ",
+  18: "stuvwxyzabcdefghijklmnopqrSTUVWXYZABCDEFGHIJKLMNOPQR",
+  19: "tuvwxyzabcdefghijklmnopqrsTUVWXYZABCDEFGHIJKLMNOPQRS",
+  20: "uvwxyzabcdefghijklmnopqrstUVWXYZABCDEFGHIJKLMNOPQRST",
+  21: "vwxyzabcdefghijklmnopqrstuVWXYZABCDEFGHIJKLMNOPQRSTU",
+  22: "wxyzabcdefghijklmnopqrstuvWXYZABCDEFGHIJKLMNOPQRSTUV",
+  23: "xyzabcdefghijklmnopqrstuvwXYZABCDEFGHIJKLMNOPQRSTUVW",
+  24: "yzabcdefghijklmnopqrstuvwxYZABCDEFGHIJKLMNOPQRSTUVWX",
+  25: "zabcdefghijklmnopqrstuvwxyZABCDEFGHIJKLMNOPQRSTUVWXY",
+  26: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 }
 
 func TestEncryptPlaintext(t *testing.T) {
-	for key, text := range expectedEncryptionResults {
-		ciphertext := EncryptPlaintext(testingString, key)
-		if strings.EqualFold(ciphertext, text) == false {
-			t.Errorf("Encryption failed! Encrypting [%s] with key [%d] produced [%s]. Should produce [%s]", testingString, key, ciphertext, text)
-		}
-	}
+  for key, text := range expectedEncryptionResults {
+    ciphertext := caesar.EncryptPlaintext(testingString, key)
+    if strings.EqualFold(ciphertext, text) == false {
+      t.Errorf("Encryption failed! Encrypting [%s] with key [%d] produced [%s]. Should produce [%s]", testingString, key, ciphertext, text)
+    }
+  }
 }
 
 func TestDecryptCiphertext(t *testing.T) {
-	for key, text := range expectedEncryptionResults {
-		plaintext := DecryptCiphertext(text, key)
-		if strings.EqualFold(plaintext, testingString) == false {
-			t.Errorf("Decryption failed! Decrypting [%s] with key [%d] produced [%s]. Should produce [%s]", text, key, plaintext, testingString)
-		}
-	}
+  for key, text := range expectedEncryptionResults {
+    plaintext := caesar.DecryptCiphertext(text, key)
+    if strings.EqualFold(plaintext, testingString) == false {
+      t.Errorf("Decryption failed! Decrypting [%s] with key [%d] produced [%s]. Should produce [%s]", text, key, plaintext, testingString)
+    }
+  }
 }
 
 func TestrotateText(t *testing.T) {
-	// If TestDecryptCiphertext TestEncryptPlaintext pass then this function must be working
+  // If TestDecryptCiphertext TestEncryptPlaintext pass then this function must be working
+}
+
+func ExampleEncryptPlaintext() {
+  ciphertext := caesar.EncryptPlaintext("Romans, go home.", 13)
+  fmt.Println(ciphertext)
+  // Output:
+  // Ebznaf, tb ubzr.
+}
+
+func ExampleDecryptCiphertext() {
+  plaintext := caesar.DecryptCiphertext("Ebznaf, tb ubzr.", 13)
+  fmt.Println(plaintext)
+  // Output:
+  // Romans, go home.
 }
