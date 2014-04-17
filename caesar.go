ab514f52e77c9ba2bf0ea5dfaa127ec5e9db94a9
Alex Jackson <alex@alexj.org>
2014-04-17 11:10:15 +0100

    Converted indentation back to tabs.
    Didn't realise “Effective Go” recommended tabs over spaces

--- caesar.go
+++ caesar.go
@@ -7,24 +7,24 @@ const upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // Encrypts a plaintext string by shifting each character with the provided key
 func EncryptPlaintext(plaintext string, key int) string {
-  return rotateText(plaintext, key)
+	return rotateText(plaintext, key)
 }
 
 // Decrypts ciphertext by reverse shifting each character in the ciphertext with the key provided
 func DecryptCiphertext(ciphertext string, key int) string {
-  return rotateText(ciphertext, -key)
+	return rotateText(ciphertext, -key)
 }
 
 func rotateText(inputText string, rot int) string {
-  rot %= 26
-  rotatedText := []byte(inputText)
+	rot %= 26
+	rotatedText := []byte(inputText)
 
-  for index, byteValue := range rotatedText {
-    if byteValue >= 'a' && byteValue <= 'z' {
-      rotatedText[index] = lowerCaseAlphabet[(int((26+(byteValue-'a')))+rot)%26]
-    } else if byteValue >= 'A' && byteValue <= 'Z' {
-      rotatedText[index] = upperCaseAlphabet[(int((26+(byteValue-'A')))+rot)%26]
-    }
-  }
-  return string(rotatedText)
+	for index, byteValue := range rotatedText {
+		if byteValue >= 'a' && byteValue <= 'z' {
+			rotatedText[index] = lowerCaseAlphabet[(int((26+(byteValue-'a')))+rot)%26]
+		} else if byteValue >= 'A' && byteValue <= 'Z' {
+			rotatedText[index] = upperCaseAlphabet[(int((26+(byteValue-'A')))+rot)%26]
+		}
+	}
+	return string(rotatedText)
 }
